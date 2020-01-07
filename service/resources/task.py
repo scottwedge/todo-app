023a1fcc6df22923c1c6b105d6a43b9aab738dca
ulrich201 <aleksije.micic@protonmail.com>
2020-01-07 18:51:27 +0100

    Implement PATCH for tasks

--- service/resources/task.py
+++ service/resources/task.py
@@ -1,8 +1,9 @@
 from flask import request
+from sqlalchemy.exc import SQLAlchemyError
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required
 
-from util import generate_message_json
+from util import generate_message_json, check_attr
 from http_status_code import HttpStatusCode
 from models.task import TaskModel
 from schemas.task import TaskSchema
@@ -12,6 +13,8 @@ task_list_schema = TaskSchema(many=True)
 
 # Response message
 TASK_NOT_FOUND = "Task not found."
+FIELD_CANNOT_BE_EDITED = "The {} cannot be edited."
+INVALID_ATTR = "Invalid attributes and/or invalid values for specfied attrubutes."
 
 
 class TaskListResource(Resource):
@@ -45,6 +48,45 @@ class TaskResource(Resource):
             return task_schema.dump(task), HttpStatusCode.OK.value
 
         return generate_message_json(HttpStatusCode.NOT_FOUND.value, TASK_NOT_FOUND)
+
+    @classmethod
+    @jwt_required
+    def patch(cls, task_id: int):
+        json = request.get_json()
+
+        # Check if the task exists
+        task = TaskModel.find_by_id(task_id)
+        if not task:
+            return generate_message_json(HttpStatusCode.NOT_FOUND.value, TASK_NOT_FOUND)
+
+        # Check if client is trying to edit readonly fields
+        readonly = {"id", "category_id"}
+        keys = json.keys()
+        forbidden = readonly & keys
+
+        if forbidden:
+            return generate_message_json(
+                HttpStatusCode.BAD_REQUEST.value,
+                FIELD_CANNOT_BE_EDITED.format(str(forbidden)[1:-1]),
+            )
+
+        # Check if the client specified non existing attrs
+
+        if check_attr(json.keys(), task):
+            try:
+                for key, value in json.items():
+                    setattr(task, key, value)
+
+                task.save_to_db()  # Persist chages to db
+            except SQLAlchemyError:
+                # NOTE: This isn't a good error message in that it lacks proper information
+                return generate_message_json(
+                    HttpStatusCode.BAD_REQUEST.value, INVALID_ATTR
+                )
+
+            return task_schema.dump(task), HttpStatusCode.OK.value
+
+        return generate_message_json(HttpStatusCode.BAD_REQUEST.value, INVALID_ATTR)
 
     @classmethod
     @jwt_required
--- service/util.py
+++ service/util.py
@@ -1,5 +1,17 @@
+from typing import List, Any
+
 # TODO: Improve so that it can take any number of K:V pairs
 
 
 def generate_message_json(http_status_code, value, key="message"):
     return {key: value}, http_status_code
+
+
+def check_attr(attrs: List[str], obj: Any) -> bool:
+    for attr in attrs:
+        try:
+            getattr(obj, attr)
+        except AttributeError:
+            return False
+
+    return True
